ce6cca23e8d4bb4ddd5d2851122c47527eddf6df
tanphatngo12 <tanphat2002125@gmail.com>
2023-03-19 10:08:05 +0700

    edit code

--- bt/Program.cs
+++ bt/Program.cs
@@ -15,7 +15,13 @@ namespace bt
             Console.WriteLine("Nhap so nguyen b ");
             int b = int.Parse(Console.ReadLine());
             Console.WriteLine("so a la {0}, so b la {1}",a,b);
-            float thuong = (a*1.0f) / b;
+            float thuong = (float) a / b;
+            int tong = a + b;
+            int hieu = a - b;
+            int tich = a * b;
+            Console.WriteLine("tong la: {0}",tong);
+            Console.WriteLine("hieu la: {0}", hieu);
+            Console.WriteLine("tich la: {0}", tich);
             Console.WriteLine("thuong la:{0} ", thuong);
             Console.ReadLine();
         }
